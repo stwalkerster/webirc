590e6046ea3691e201bc4a33eef9949f01c6456c
Dean Thomson <deanthomson92@gmail.com>
2013-03-01 13:54:24 +0000

    overview

--- overview.php
+++ overview.php
@@ -9,9 +9,9 @@
 	
 	<h2>Pages</h2>
 	<ul>
-		<li><a href="index.php">Login form</a></li>
-		<li><a href="client.php">Client form</a></li>
-        <li><a href="GuidePage.php">Guide page</a></li>
+		<li><a href="index.php">Login form</a> (David Sturrock) - Allows the user to provide the information required to connect to an IRC server and start chatting. This information is passed to the IRC client.</li>
+		<li><a href="client.php">Client form</a> (Simon Walker) - Displays the Java applet which will be used as the IRC client.</li>
+        <li><a href="GuidePage.php">Guide page</a> (Ximeng Bao) - Provide the user with information on using this application.</li>
 	</ul>
 	<h2>Other files</h2>
 	<ul>
