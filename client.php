10d2b2219a01a46c0f647ed76628e454035f0eb1
Simon Walker <github@stwalkerster.co.uk>
2013-03-01 12:58:25 +0000

    Common header.
    Needs adding on GuidePage, index, overview

--- client.php
+++ client.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-<title>IRC Client</title>
-<link rel="stylesheet" href="style.css" />
-</head>
-<body>
-<div id="header"><h1>IRC Client</h1></div>
+<?php include("head.inc.php"); createHeader("IRC Client"); ?>
 <div id="content"><div id="applet"><object type="application/x-java-applet"
     name="irc" width="800" height="600">
     <param name="codebase" value="java/bin" />
@@ -15,5 +7,4 @@
     <param name="mayscript" value="true" />
 </object></div>
 </div>
-
 <?php include("foot.inc.php");?>
