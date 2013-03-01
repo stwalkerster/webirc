2af310a21a7dc0b1f5aacfb25e80583a797f9fe6
Simon Walker <github@stwalkerster.co.uk>
2013-03-01 13:50:04 +0000

    design text

--- overview.php
+++ overview.php
@@ -4,6 +4,9 @@
 <p>
 	Our project is to build a web IRC client. This client is being implemented as a parameterised Java applet embedded within XHTML pages.
 </p>
+<p>
+	The design is a web-based login form which then passes parameters into a Java Applet running on a different page. The IRC client itself will be implemented in the Java Applet. The login form has all the information required to connect to any IRC server and authenticate to network services if requested, using a specific account name and password (which could be different from the nickname the user has requested to use).
+</p>
 	<h2>Group Members</h2>
 	<ul><li>Ximeng Bao</li><li>David Sturrock</li><li>Dean Thomson</li><li>Simon Walker</li></ul>
 	
