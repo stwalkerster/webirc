598dac2e2d6f06c78fab0b5ff8b115aa74e61d33
Simon Walker <github@stwalkerster.co.uk>
2013-03-01 12:53:09 +0000

    common footer

--- client.php
+++ client.php
@@ -15,22 +15,5 @@
     <param name="mayscript" value="true" />
 </object></div>
 </div>
-<div id="footer">
-	<div id="foot-left">
-		<p>
-      <a href="http://validator.w3.org/check?uri=referer"><img
-          src="http://www.w3.org/Icons/valid-xhtml10-blue"
-          alt="Valid XHTML 1.0!" height="31" width="88" /></a>
-	  <a href="http://jigsaw.w3.org/css-validator/check/referer">
-        <img style="border:0;width:88px;height:31px"
-            src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
-            alt="Valid CSS!" />
-    </a>
-    </p>
-	</div>
-	<div id="foot-right">
-		<p>Developed by: X Bao, D Sturrock, D Thomson and S Walker</p>
-	</div>
-</div>
-</body>
-</html>
+
+<?php include("foot.inc.php");?>
